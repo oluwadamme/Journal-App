f2861952cbf7c41b3c57be06973d3c23e982bc4d
Damilola Adeniyi <adeniiyidamilola246@gmail.com>
2024-01-21 22:59:51 +0100

    populate data on journal page

--- app/src/main/java/com/example/journalapp/JournalList.kt
+++ app/src/main/java/com/example/journalapp/JournalList.kt
@@ -1,10 +1,13 @@
 package com.example.journalapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.journalapp.databinding.ActivityJournalListBinding
@@ -12,6 +15,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.toObject
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.StorageReference
 
@@ -20,12 +24,12 @@ class JournalList : AppCompatActivity() {
     private val collectionName="Journals"
     // firebase instances
     private lateinit var user:FirebaseUser
-    private lateinit var auth: FirebaseAuth
+    private lateinit var firebaseAuth: FirebaseAuth
    private val db= FirebaseFirestore.getInstance()
     val collectionReference=db.collection(collectionName)
     private lateinit var storage:StorageReference
 
-    private lateinit var journalList: List<Journal>
+    private lateinit var journalList: MutableList<Journal>
     private lateinit var adapter: JournalRecyclerAdapter
 
     private lateinit var noPostTextView: TextView
@@ -34,8 +38,8 @@ class JournalList : AppCompatActivity() {
         binding=DataBindingUtil.setContentView(this,R.layout.activity_journal_list)
 
         //firebase auth
-        auth=Firebase.auth
-        user=auth.currentUser!!
+        firebaseAuth=Firebase.auth
+        user=firebaseAuth.currentUser!!
 
         // recyclerView
         binding.recyclerView.setHasFixedSize(true)
@@ -49,14 +53,40 @@ class JournalList : AppCompatActivity() {
         return super.onCreateOptionsMenu(menu)
     }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        when(item.itemId){
-//            R.id.action_add->if (auth !=null) {
-//
-//            }
-//            }
-//
-//
-//        }
+        when(item.itemId){
+            R.id.action_add->{
+                val intent=Intent(this,AddJournalActivity::class.java)
+               startActivity(intent)
+            }
+            R.id.action_sign_out->{
+
+                firebaseAuth.signOut()
+                val intent=Intent(this,MainActivity::class.java)
+                startActivity(intent)
+            }
+
+        }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        collectionReference.whereEqualTo("userId",JournalUser.instance?.userId)
+            .get().addOnSuccessListener { it ->
+                if (!it.isEmpty){
+                    it.forEach {
+                      val journal:Journal=  it.toObject<Journal>()
+                        journalList.add(journal)
+                    }
+                    adapter=JournalRecyclerAdapter(journalList)
+                    binding.recyclerView.adapter=adapter
+                    adapter.notifyDataSetChanged()
+                }else{
+                    binding.emptyListText.visibility=View.VISIBLE
+                }
+
+            }.addOnFailureListener {
+                Toast.makeText(this,"Opps, something well wrong",Toast.LENGTH_SHORT).show()
+            }
     }
 }
